73bd76b47ae374ba7116a47d19da8f414c21d21b
Eric Cabrel <tericcabrel@yahoo.com>
2019-10-06 12:14:35 +0100

    Initial commit

---
+++ src/main/java/com/tericcabrel/passify/PassifyApplication.java
@@ -0,0 +1,13 @@
+package com.tericcabrel.passify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassifyApplication.class, args);
+    }
+
+}
