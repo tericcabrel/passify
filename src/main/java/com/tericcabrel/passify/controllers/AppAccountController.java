c33e3cb94d9d3fd65811b44ba64582dccbed337e
Eric Cabrel <tericcabrel@yahoo.com>
2019-12-08 18:29:50 +0100

    Define files and structures to create pages of Bank accounts

--- src/main/java/com/tericcabrel/passify/controllers/AppAccountController.java
+++ src/main/java/com/tericcabrel/passify/controllers/AppAccountController.java
@@ -7,12 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppAccountController {
     @GetMapping("/accounts")
-    public String list(Model model) {
+    public String listAppAccount(Model model) {
         return "app/user/accounts/list";
     }
 
     @GetMapping("/accounts/create")
-    public String create(Model model) {
+    public String createAppAccount(Model model) {
         return "app/user/accounts/create";
+    }
+
+    @GetMapping("/bank-accounts")
+    public String listBankAccount(Model model) {
+        return "app/user/bank-accounts/list";
+    }
+
+    @GetMapping("/bank-accounts/create")
+    public String createBankAccount(Model model) {
+        return "app/user/bank-accounts/create";
     }
 }
